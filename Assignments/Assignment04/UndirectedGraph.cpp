61eeb809812d49c3c83eab171f5082484af5cdd1
xizbow <93213703+xizbow1@users.noreply.github.com>
2024-04-16 16:16:40 -0700

    Assignment 4 Done

--- Assignments/Assignment04/UndirectedGraph.cpp
+++ Assignments/Assignment04/UndirectedGraph.cpp
@@ -12,14 +12,14 @@ using namespace std;
 
 UndirectedGraph::UndirectedGraph(){
     for(int i = DEFAULTSIZE; i > 0; i--){
-        vector<bool> temp(i, false);
+        vector<bool> temp(DEFAULTSIZE, false);
         graph.push_back(temp);
     }
 }
 
 UndirectedGraph::UndirectedGraph(int numVertices){
     for(int i = numVertices; i > 0; i--){
-        vector<bool> temp(i, false);
+        vector<bool> temp(numVertices, false);
         graph.push_back(temp);
     }
 }
@@ -29,61 +29,69 @@ int UndirectedGraph::vertexCount(){
 }
 
 bool UndirectedGraph::edgeExists(int vert1, int vert2){
-    if(vert1 >= graph.size() || vert2 >= graph.size()){ // Immediately returns false if either number is higher than the number of vertices
+    vert1--; // Off by one :)
+    vert2--;
+    if(vert1 >= graph.size() || vert2 >= graph.size() || vert2 >= graph[vert1].size()){ // Immediately returns false if either number is higher than the number of vertices
         return false;
     }
-    if(vert2 < bool UndirectedGraph::edgeExists(int vert1, int vert2)){
-        if(vert1 >= graph.size() || vert2 >= graph.size() || vert2 >= graph[vert1].size()){ 
-            return false;
-        }
-        if(vert2 < vert1){ 
-            swap(vert1, vert2);
-        }
+    if(vert2 < vert1){
+        swap(vert1, vert2);
     }
     return graph.at(vert1).at(vert2);
 }
 
 bool UndirectedGraph::addEdge(int vert1, int vert2){
+    vert1--; // Off by one :)
+    vert2--;
     if(vert1 >= graph.size() || vert2 >= graph.size() || vert2 >= graph[vert1].size()){ 
         return false;
     }
     if(vert2 < vert1){ 
         swap(vert1, vert2);
     }
-    graph.at(vert1).at(vert2) = true;
-    return true;
-}vert1){ // Make sure vert2 is always bigger, because we're using an upper triangle
-        swap(vert1, vert2);
-    }
-    return graph.at(vert1).at(vert2);
-}
-
-bool UndirectedGraph::addEdge(int vert1, int vert2){
-    if(vert1 >= graph.size() || vert2 >= graph.size()){ // Immediately returns false if either number is higher than the number of vertices 
-        return false;
-    }
-    if(vert2 < vert1){ // Make sure vert2 is always bigger, because we're using an upper triangle
-        swap(vert1, vert2);
-    }
     if(graph.at(vert1).at(vert2) == true){
         return false;
-    } else {
-        graph.at(vert1).at(vert2) = true;
-        return true;
     }
+    graph.at(vert1).at(vert2) = true;
+    return true;
 }
 
 bool UndirectedGraph::removeEdge(int vert1, int vert2){
-    if(vert1 >= graph.size() || vert2 >= graph.size()){ // Immediately returns false if either number is higher than the number of vertices 
+    vert1--; // Off by one :)
+    vert2--;
+    if(vert1 >= graph.size() || vert2 >= graph.size() || vert2 >= graph.at(vert1).size()){ 
         return false;
     }
-    if(vert2 < vert1){ // Make sure vert2 is always bigger, because we're using an upper triangle
+    if(vert2 < vert1){ 
         swap(vert1, vert2);
     }
     if(graph.at(vert1).at(vert2) == false){
         return false;
-    } else {
-        graph.at(vert1).at(vert2) = false;
-        return true;
     }
+    graph.at(vert1).at(vert2) = false;
+    return true;
+}
+
+bool UndirectedGraph::graphConnected(){
+    vector<bool> visited(graph.size(), false);
+    vector<int> stack;
+    stack.push_back(0);
+    while(!stack.empty()){
+        int current = stack.back();
+        stack.pop_back();
+        if(visited.at(current) == false){
+            visited.at(current) = true;
+            for(int i = 0; i < graph.at(current).size(); i++){
+                if(graph.at(current).at(i) == true){
+                    stack.push_back(i);
+                }
+            }
+        }
+    }
+    for(int i = 0; i < visited.size(); i++){
+        if(visited.at(i) == false){
+            return false;
+        }
+    }
+    return true;
 }
--- Assignments/Assignment04/main.cpp
+++ Assignments/Assignment04/main.cpp
@@ -7,9 +7,10 @@
 using namespace std;
 
 int main(){
-    UndirectedGraph myGraph = UndirectedGraph();
+    UndirectedGraph myGraph = UndirectedGraph(5);
+
     myGraph.addEdge(1, 3);
-    myGraph.addEdge(2, 4);
+    myGraph.addEdge(4, 2);
     myGraph.addEdge(5, 3);
     assert(myGraph.edgeExists(3, 1)); // True
     assert(!myGraph.edgeExists(3, 2)); // False
@@ -17,4 +18,21 @@ int main(){
     assert(myGraph.edgeExists(3, 5)); // True
     assert(myGraph.edgeExists(4, 2)); // True
     cout << "addEdge tests complete!" << endl;
+
+    myGraph.removeEdge(3, 5);
+    myGraph.removeEdge(4, 2);
+    assert(!myGraph.edgeExists(5, 3));
+    assert(!myGraph.edgeExists(2, 4));
+    cout << "removeEdge tests complete!" << endl;
+
+    assert(myGraph.vertexCount() == 5);
+    cout << "vertexCount test complete!" << endl;
+
+    assert(!myGraph.graphConnected());
+    myGraph.addEdge(1, 2);
+    myGraph.addEdge(2, 3);
+    myGraph.addEdge(3, 4);
+    myGraph.addEdge(4, 5);
+    assert(myGraph.graphConnected());
+    cout << "graphConnected tests complete!" << endl;
 }
